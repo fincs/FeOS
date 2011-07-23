cf8682bc6111dc41bb1142f54193d90e6155a1a1
fincs <fernandoincs@hotmail.com>
2011-07-23 17:59:36 +0200

    Add input test example to FeOS SDK

---
+++ sdk/examples/inputtest/source/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <feos.h>
+
+void PrintStatus(keys_t keys, const char* verb)
+{
+	const char* name = NULL;
+	     if (keys & KEY_A) name = "A";
+	else if (keys & KEY_B) name = "B";
+	else if (keys & KEY_X) name = "X";
+	else if (keys & KEY_Y) name = "Y";
+	else if (keys & KEY_L) name = "L";
+	else if (keys & KEY_R) name = "R";
+	else if (keys & KEY_UP) name = "Up";
+	else if (keys & KEY_DOWN) name = "Down";
+	else if (keys & KEY_LEFT) name = "Left";
+	else if (keys & KEY_RIGHT) name = "Right";
+	else if (keys & KEY_START) name = "Start";
+	else if (keys & KEY_SELECT) name = "Select";
+	else if (keys & KEY_TOUCH) name = "The stylus";
+	if (name) printf("%s was just %sed!\n", name, verb);
+}
+
+int main()
+{
+	printf("Button test (START=exit)\n");
+	for(;;)
+	{
+		FeOS_WaitForVBlank();
+		PrintStatus(FeOS_GetKeysDown(), "press");
+		PrintStatus(FeOS_GetKeysUp(), "releas");
+
+		if(FeOS_GetKeysHeld() & KEY_TOUCH)
+		{
+			styluspos_t st;
+			FeOS_GetStylusPos(&st);
+			printf("Stylus: (%d, %d)\n", st.x, st.y);
+		}
+
+		if(FeOS_GetKeysDown() & KEY_START) break;
+	}
+	return 0;
+}
