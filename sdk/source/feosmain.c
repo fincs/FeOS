b80ea2277bdb84f4cbefab27628d399fa6ea4ec9
fincs <fernandoincs@hotmail.com>
2011-08-08 16:23:35 +0200

    Detect multiple instances, backup the exit jump buffer if not the first

--- sdk/source/feosmain.c
+++ sdk/source/feosmain.c
@@ -1,6 +1,8 @@
 #include <feos.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <setjmp.h>
+#include <alloca.h>
 
 word_t FEOS_WEAK FeOSMain(word_t, word_t, word_t, word_t);
 int FEOS_WEAK main(int, const char*[]);
@@ -11,6 +13,7 @@ void this_exit(int);
 
 jmp_buf __exit_buf;
 volatile int __exit_done;
+volatile int __ninstances = 0;
 
 word_t __FeOSMain(word_t event, word_t prm1, word_t prm2, word_t prm3)
 {
@@ -25,18 +28,38 @@ word_t __FeOSMain(word_t event, word_t prm1, word_t prm2, word_t prm3)
 			return FEOS_RC_OK;
 
 		case FEOS_EP_MAIN:
+		{
+			word_t* bk_buf = NULL;
+
 			if (!main) return FEOS_RC_ERR;
+
+			if (__ninstances)
+			{
+				// Backup the exit jump buffer
+				bk_buf = alloca(sizeof(jmp_buf));
+				memcpy(bk_buf, __exit_buf, sizeof(jmp_buf));
+			}
 
 			if (!FeOS_PushExitFunc(this_exit)) return FEOS_RC_ERR;
 
+			__ninstances ++;
 			__exit_done = 0;
 			int rc = setjmp(__exit_buf);
 
-			if (__exit_done) return (word_t) rc;
+			if (__exit_done)
+			{
+				__ninstances --;
+				if (bk_buf) memcpy(__exit_buf, bk_buf, sizeof(jmp_buf));
+				return (word_t) rc;
+			}
 
 			word_t rc2 = (word_t) main((int) prm1, (const char**) prm2);
 			FeOS_PopExitFunc();
+
+			__ninstances --;
+			if (bk_buf) memcpy(__exit_buf, bk_buf, sizeof(jmp_buf));
 			return rc2;
+		}
 
 		default:
 			if (FeOSMain) return FeOSMain(event, prm1, prm2, prm3);
