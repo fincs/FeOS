7fb25ac50ee5cfae18e8526a6db7c29c00054583
fincs <fernandoincs@hotmail.com>
2011-08-11 18:20:45 +0200

    Add entrypoint function for finding the exception index table

--- sdk/include/feosuser.h
+++ sdk/include/feosuser.h
@@ -11,7 +11,14 @@
 
 #define FEOS_EP_INIT 0x00000001
 #define FEOS_EP_FINI 0x00000002
+#define FEOS_EP_GETEXIDXTBL 0x00000003
 #define FEOS_EP_MAIN 0x00000100
+
+typedef struct
+{
+	void* table;
+	int nentries;
+} FeOS_GetExidxTbl_t;
 
 #define FEOS_RC_OK 0
 #define FEOS_RC_ERR 0xFFFFFFFF
