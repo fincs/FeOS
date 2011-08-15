839563e2aede92b35b4e4f285a10aa117062687f
fincs <fernandoincs@hotmail.com>
2011-08-15 20:47:37 +0200

    Fix typo introduced by 565fa3b

--- sdk/bin/fxe2tool_src/source/main.c
+++ sdk/bin/fxe2tool_src/source/main.c
@@ -167,7 +167,7 @@ int PrerelocateSection(elf2fx2_cnvstruct_t* cs, word_t vsect, byte_t* sect, Elf3
 			}
 
 			word_t offTopBits = offFromTrg >> 28;
-			if (offTopBits == 0x0 || offTopBits == 0xF)
+			if (offTopBits != 0x0 && offTopBits != 0xF)
 				die("Offset from non-function import target too big!");
 
 			*rtarget |= offFromTrg << 4;

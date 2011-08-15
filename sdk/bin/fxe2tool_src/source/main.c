fincs/FeOS

commit 565fa3ba6a6ff6adb85e104a8969b3a865a13526

Fix negative non-function relocation addendums

sdk/bin/fxe2tool_src/source/main.c
@@ -166,7 +166,8 @@ int PrerelocateSection(elf2fx2_cnvstruct_t* cs, word_t vsect, byte_t* sect, Elf3
 				die("Non-supported type non-function imports are not supported!");
 			}
 
-			if (offFromTrg & 0xF0000000)
+			word_t offTopBits = offFromTrg >> 28;
+			if (offTopBits == 0x0 || offTopBits == 0xF)
 				die("Offset from non-function import target too big!");
 
 			*rtarget |= offFromTrg << 4;

commit 839563e2aede92b35b4e4f285a10aa117062687f

Fix typo introduced by 565fa3b

sdk/bin/fxe2tool_src/source/main.c
@@ -167,7 +167,7 @@ int PrerelocateSection(elf2fx2_cnvstruct_t* cs, word_t vsect, byte_t* sect, Elf3
 			}
 
 			word_t offTopBits = offFromTrg >> 28;
-			if (offTopBits == 0x0 || offTopBits == 0xF)
+			if (offTopBits != 0x0 && offTopBits != 0xF)
 				die("Offset from non-function import target too big!");
 
 			*rtarget |= offFromTrg << 4;