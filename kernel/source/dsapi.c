014c0cfe1471d20692413c5edbe065fa914dc9bb
fincs <fernandoincs@hotmail.com>
2011-10-07 21:11:56 +0200

    Expose IRQ functions

--- kernel/source/dsapi.c
+++ kernel/source/dsapi.c
@@ -15,7 +15,12 @@ BEGIN_TABLE(FEOSDSAPI)
 	ADD_FUNC_ALIAS(keysDown, FeOS_GetKeysDown),
 	ADD_FUNC_ALIAS(keysHeld, FeOS_GetKeysHeld),
 	ADD_FUNC_ALIAS(keysUp, FeOS_GetKeysUp),
-	ADD_FUNC(FeOS_GetStylusPos)
+	ADD_FUNC(FeOS_GetStylusPos),
+	ADD_FUNC(FeOS_SetInterrupt),
+	ADD_FUNC(FeOS_CheckPendingIRQs),
+	ADD_FUNC(FeOS_WaitForIRQ),
+	ADD_FUNC_ALIAS(FeOS_swi_IrqDisable, FeOS_IrqDisable),
+	ADD_FUNC_ALIAS(FeOS_swi_IrqEnable, FeOS_IrqEnable)
 END_TABLE(FEOSDSAPI)
 
 MAKE_FAKEMODULE(FEOSDSAPI)
--- sdk/include/feosdsapi.h
+++ sdk/include/feosdsapi.h
@@ -26,7 +26,23 @@ enum
 	KEY_TOUCH  = BIT(12)  // Touchscreen pen down
 };
 
+enum
+{
+	IRQ_VBLANK = BIT(0), // Vertical blank
+	IRQ_HBLANK = BIT(1), // Horizontal blank
+	IRQ_TIMER0 = BIT(3), // Timer 0
+	IRQ_TIMER1 = BIT(4), // Timer 1
+	IRQ_TIMER2 = BIT(5), // Timer 2
+	IRQ_TIMER3 = BIT(6), // Timer 3
+};
+
 keys_t FeOS_GetKeysDown();
 keys_t FeOS_GetKeysHeld();
 keys_t FeOS_GetKeysUp();
 void FeOS_GetStylusPos(styluspos_t*);
+
+fp_t FeOS_SetInterrupt(word_t, fp_t);
+word_t FeOS_CheckPendingIRQs();
+void FeOS_WaitForIRQ(word_t);
+void FeOS_IrqEnable(word_t);
+void FeOS_IrqDisable(word_t);
