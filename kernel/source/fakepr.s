63596a465d648ef384a224c9e35681976d0520c1
fincs <fernandoincs@hotmail.com>
2012-04-02 19:34:43 +0200

    Kernel: use fake PR routines; see previous commit for details

---
+++ kernel/source/fakepr.s
@@ -0,0 +1,11 @@
+@ Fake placeholder personality routines
+.text
+.global __aeabi_unwind_cpp_pr0
+.hidden __aeabi_unwind_cpp_pr0
+.global __aeabi_unwind_cpp_pr1
+.hidden __aeabi_unwind_cpp_pr1
+.global __aeabi_unwind_cpp_pr2
+.hidden __aeabi_unwind_cpp_pr2
+__aeabi_unwind_cpp_pr0:
+__aeabi_unwind_cpp_pr1:
+__aeabi_unwind_cpp_pr2:
