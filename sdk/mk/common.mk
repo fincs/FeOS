6b65efb7564684538ef6630a406b16711321d083
fincs <fernandoincs@hotmail.com>
2012-04-02 19:34:07 +0200

    SDK: many debugging-related changes, see details
    - Debugging information generation is turned on by default on ARM9
      (includes stack backtracing information)
    - common.mk: now accepts:
        - CONF_DEBUG = 1 (turn off optimizations, define DEBUG)
        - CONF_NODEBUGINFO = 1 (turn off dbg info gen)
        - CONF_USECXXLIB = 1 (link to C++ lib)
    - fxe2.ld: support debugging sections
    - libfeos: add empty fake personality routines to avoid dependency on
      the C++ lib. This works even if C++ is enabled because they aren't
      actually referenced; instead a function ID (0..2) is written into
      the stack unwinding information. The dependency exists because GCC
      creates a R_ARM_NONE (aka NOP) relocation to the personality routine,
      as dictated by ARM EHABI, section 6.3.

--- sdk/mk/common.mk
+++ sdk/mk/common.mk
@@ -18,6 +18,7 @@ export CXX     := $(PREFIX)g++
 export AS      := $(PREFIX)as
 export AR      := $(PREFIX)ar
 export OBJCOPY := $(PREFIX)objcopy
+export STRIP   := $(PREFIX)strip
 
 #---------------------------------------------------------------------------------
 # options for code generation
@@ -31,6 +32,11 @@ INCLUDECXX := -I$(FEOSSDK)/include/cxx
 else
 ARCH      := -mcpu=arm7tdmi -mtune=arm7tdmi
 DEFINES   += -DARM7
+LIBFEOSSUF := 7
+endif
+
+ifeq ($(CONF_GCSECTIONS),1)
+GCSECTIONS := --gc-sections,
 endif
 
 ARMARCH   := -marm
@@ -39,8 +45,45 @@ ifeq ($(strip $(DEFARCH)),)
 DEFARCH   := $(THUMBARCH)
 endif
 
-CFLAGS   := -g0 -Wall -O2 -save-temps -fvisibility=hidden\
-            $(ARCH) -fomit-frame-pointer -ffast-math\
+export CONF_DEBUG
+ifeq ($(CONF_DEBUG),1)
+ifeq ($(CONF_ARM7),1)
+$(error "Debug build not available on ARM7, sorry")
+endif
+CONF_NODEBUGINFO :=
+CFLAGSBASE := -O0
+DEFINES    += -DDEBUG
+else
+CFLAGSBASE := -O2
+DEFINES    += -DNDEBUG
+endif
+
+# The default is to produce debugging info on ARM9 (but not on ARM7)
+ifneq ($(CONF_ARM7),1)
+export EMITDEBUGINFO := 1
+else
+export EMITDEBUGINFO := 0
+endif
+
+ifeq ($(CONF_NODEBUGINFO),1)
+export EMITDEBUGINFO := 0
+endif
+
+ifeq ($(EMITDEBUGINFO),1)
+DEBUGFLAG   := -g
+CEXTRAFLAGS := -funwind-tables
+else
+DEBUGFLAG   := -g0
+endif
+
+ifeq ($(CONF_USECXXLIB),1)
+ifeq ($(CONF_ARM7),1)
+$(error "Debug build not available on ARM7, sorry")
+endif
+export CXXLIB := -lfeoscxx
+endif
+
+CFLAGS   := $(ARCH) $(DEBUGFLAG) $(CFLAGSBASE) $(CEXTRAFLAGS) -fomit-frame-pointer -Wall -save-temps -fvisibility=hidden\
             -mthumb-interwork -mword-relocations $(INCLUDE) $(DEFINES) -nostdinc $(CONF_CFLAGS)
 
 CXXFLAGS := $(CFLAGS) $(INCLUDECXX) -fno-rtti -nostdinc++ -fvisibility-inlines-hidden $(CONF_CXXFLAGS)
@@ -49,17 +92,11 @@ ifneq ($(CONF_ARM7),)
 CXXFLAGS += -fno-exceptions
 endif
 
-ASFLAGS  := -g0 $(ARCH) $(DEFINES) $(INCLUDE)
-LDFLAGS  := -nostartfiles -nostdlib -T $(FEOSBIN)/fxe2.ld -g $(ARCH) -Wl,-d,-q,--pic-veneer,-Map,$(TARGET).map
-
-ifeq ($(CONF_ARM7),)
-DEFLIB := -lfeos
-else
-DEFLIB := -lfeos7
-endif
+ASFLAGS  := $(DEBUGFLAG) $(ARCH) $(DEFINES) $(INCLUDE)
+LDFLAGS  := -nostartfiles -nostdlib -T $(FEOSBIN)/fxe2.ld $(ARCH) -Wl,-d,-q,$(GCSECTIONS)--pic-veneer,-Map,$(TARGET).map
 
 ifeq ($(CONF_NOSTDLIB),)
-LIBS     := $(CXXLIB) $(DEFLIB)
+LIBS     := -lfeos$(LIBFEOSSUF) $(CXXLIB)
 endif
 LIBS     := $(CONF_LIBS) $(LIBS)
 
@@ -155,7 +192,7 @@ endif
 #---------------------------------------------------------------------------------
 clean:
 ifneq ($(strip $(CONF_TARGET)),staticlib)
-	@rm -fr $(BUILD) $(TARGET).elf $(TARGET).fx2 $(TARGET).imp.a lib/lib$(TARGET).a
+	@rm -fr $(BUILD) $(TARGET).elf $(TARGET).elf.dbg $(TARGET).fx2 $(TARGET).imp.a lib/lib$(TARGET).a
 else
 	@rm -fr $(BUILD) lib/lib$(TARGET).a
 endif
@@ -184,6 +221,10 @@ $(OUTPUT).elf: $(OFILES)
 	@echo
 	@echo Linking...
 	@$(LD) $(LDFLAGS) $(OFILES) $(LIBPATHS) $(LIBS) -o $@
+ifeq ($(EMITDEBUGINFO),1)
+	@$(OBJCOPY) --only-keep-debug $@ $@.dbg
+	@$(STRIP) -g $@
+endif
 
 else
 
