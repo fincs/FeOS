dc800bfdec2628f90e0a3926b51857c7ff3edc3e
fincs <fernandoincs@hotmail.com>
2012-06-09 13:52:23 +0200

    Unb0rk fartool stdout writing on Linux (thanks to kouki)

--- sdk/bin/fartool_src/source/FileClass.h
+++ sdk/bin/fartool_src/source/FileClass.h
@@ -6,13 +6,28 @@ class FileClass
 {
 	FILE* f;
 	bool LittleEndian, own;
+	int filePos;
+
+	size_t _RawRead(void* buffer, size_t size)
+	{
+		size_t x = fread(buffer, 1, size, f);
+		filePos += x;
+		return x;
+	}
+
+	size_t _RawWrite(const void* buffer, size_t size)
+	{
+		size_t x = fwrite(buffer, 1, size, f);
+		filePos += x;
+		return x;
+	}
 
 public:
-	FileClass(const char* file, const char* mode) : LittleEndian(true), own(true)
+	FileClass(const char* file, const char* mode) : LittleEndian(true), own(true), filePos(0)
 	{
 		f = fopen(file, mode);
 	}
-	FileClass(FILE* inf) : f(inf), LittleEndian(true), own(false) { }
+	FileClass(FILE* inf) : f(inf), LittleEndian(true), own(false), filePos(0) { }
 	~FileClass()
 	{
 		if (f && own) fclose(f);
@@ -27,45 +42,45 @@ public:
 	word_t ReadWord()
 	{
 		word_t value;
-		fread(&value, sizeof(word_t), 1, f);
+		_RawRead(&value, sizeof(word_t));
 		return LittleEndian ? le_word(value) : be_word(value);
 	}
 
 	void WriteWord(word_t value)
 	{
 		value = LittleEndian ? le_word(value) : be_word(value);
-		fwrite(&value, sizeof(word_t), 1, f);
+		_RawWrite(&value, sizeof(word_t));
 	}
 
 	hword_t ReadHword()
 	{
 		hword_t value;
-		fread(&value, sizeof(hword_t), 1, f);
+		_RawRead(&value, sizeof(hword_t));
 		return LittleEndian ? le_hword(value) : be_hword(value);
 	}
 
 	void WriteHword(hword_t value)
 	{
 		value = LittleEndian ? le_hword(value) : be_hword(value);
-		fwrite(&value, sizeof(hword_t), 1, f);
+		_RawWrite(&value, sizeof(hword_t));
 	}
 
 	byte_t ReadByte()
 	{
 		byte_t value;
-		fread(&value, sizeof(byte_t), 1, f);
+		_RawRead(&value, sizeof(byte_t));
 		return value;
 	}
 
 	void WriteByte(byte_t value)
 	{
-		fwrite(&value, sizeof(byte_t), 1, f);
+		_RawWrite(&value, sizeof(byte_t));
 	}
 	
-	bool ReadRaw(void* buffer, size_t size) { return fread(buffer, 1, size, f) == size; }
-	bool WriteRaw(const void* buffer, size_t size) { return fwrite(buffer, 1, size, f) == size; }
+	bool ReadRaw(void* buffer, size_t size) { return _RawRead(buffer, size) == size; }
+	bool WriteRaw(const void* buffer, size_t size) { return _RawWrite(buffer, size) == size; }
 
-	void Seek(int pos, int mode) { fseek(f, pos, mode); }
-	int Tell() { return ftell(f); }
+	//void Seek(int pos, int mode) { fseek(f, pos, mode); }
+	int Tell() { return filePos /*ftell(f)*/; }
 	void Flush() { fflush(f); }
 };
