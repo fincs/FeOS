c6bd8c2b2e712c34463cd5c46d0beca980d6de51
fincs <fernandoincs@hotmail.com>
2012-06-22 13:10:10 +0200

    Fix __aeabi_atexit

--- sdk/source/cpp_atexit.c
+++ sdk/source/cpp_atexit.c
@@ -2,7 +2,7 @@
 
 void* __dso_handle;
 
-int __aeabi_atexit(atexitCb callback, void* obj, void* dso_handle)
+int __aeabi_atexit(void* obj, atexitCb callback, void* dso_handle)
 {
 	return __reg_atexit(callback, obj);
 }
