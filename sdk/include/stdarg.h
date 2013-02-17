b1054ae8eaae738ff4afff128a7c3dd8f0dadbac
fincs <fincs.alt1@gmail.com>
2013-02-17 12:41:41 +0100

    stdarg.h: add va_copy()

--- sdk/include/stdarg.h
+++ sdk/include/stdarg.h
@@ -10,4 +10,5 @@ typedef __builtin_va_list va_list;
 
 #define va_start(v, n) __builtin_va_start(v, n)
 #define va_arg(v, t)   __builtin_va_arg(v, t)
+#define va_copy(d, s)  __builtin_va_copy(d, s)
 #define va_end(v)      __builtin_va_end(v)
