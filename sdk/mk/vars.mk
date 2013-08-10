cfc73254c427cdeda927476502b46e1c453102e8
fincs <fincs.alt1@gmail.com>
2013-08-10 12:53:23 +0200

    vars.mk: minor tweak

--- sdk/mk/vars.mk
+++ sdk/mk/vars.mk
@@ -6,7 +6,11 @@ FEOSBIN = $(FEOSSDK)/bin
 FEOSUSERLIB = $(FEOSSDK)/userlib
 
 ifeq ($(FEOSDEST),)
+ifneq ($(wildcard $(FEOSSDK)/libfeos.mk),)
 export FEOSDEST = $(FEOSSDK)/../FeOS
+else
+export FEOSDEST = $(FEOSSDK)/install-root
+endif
 endif
 
 #---------------------------------------------------------------------------------
